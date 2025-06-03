45f70a1ce8be6b07b62d857b29c936e5d7d999af
Austin Huang <ash110@duke.edu>
2025-06-03 01:39:25 -0400

    Doc: add javadoc

--- src/main/java/oogasalad/player/view/FormattingUtils.java
+++ src/main/java/oogasalad/player/view/FormattingUtils.java
@@ -4,8 +4,17 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import oogasalad.engine.view.components.FormattingUtil;
 
+/**
+ * Utility class for displaying standardized UI dialogs.
+ */
 public class FormattingUtils {
 
+  /**
+   * Displays an error dialog with the given title and message using standard styling.
+   *
+   * @param title   the title of the error dialog
+   * @param message the message to display in the dialog
+   */
   public void showErrorDialog(String title, String message) {
     Alert alert = new Alert(AlertType.ERROR);
     alert.setTitle(title);
