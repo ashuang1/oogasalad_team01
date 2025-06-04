0f1c15dcc6ae9e1c3bcf9285c2a601274b7d4dda
Austin Huang <ash110@duke.edu>
2025-06-03 23:26:49 -0400

    Feature: add record containing localPlayerId, activePlayerIds, remoteMap

--- src/main/java/oogasalad/engine/records/MultiplayerContextRecord.java
+++ src/main/java/oogasalad/engine/records/MultiplayerContextRecord.java
@@ -1,4 +1,17 @@
 package oogasalad.engine.records;
 
-public record MultiplayerContextRecord() {
+import java.util.Map;
+import java.util.Set;
+import oogasalad.player.model.strategies.control.RemoteControlStrategy;
+
+/**
+ * A record that encapsulates the context for a multiplayer game.
+ *
+ * @param localPlayerId   playerId of local client
+ * @param activePlayerIds playerIds of active players
+ * @param remoteMap       maps playerId to RemoteControlStrategy
+ */
+public record MultiplayerContextRecord(int localPlayerId, Set<Integer> activePlayerIds,
+                                       Map<Integer, RemoteControlStrategy> remoteMap) {
+
 }
